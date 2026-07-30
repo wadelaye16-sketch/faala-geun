3ea2883c3301bda65d544e3d2bd15ca2b8e685ca
wadelaye16-sketch <wadelaye16@gmail.com>
2026-07-30 19:11:13 +0200

    Mise a jour du contenu depuis l espace de gestion

--- data/contenu.js
+++ data/contenu.js
@@ -2,7 +2,7 @@
    Contenu de l'application Touba Média.
    Fichier produit par l'espace de gestion (admin.html).
    Tu peux le modifier à la main, mais l'interface est plus sûre.
-   Dernière mise à jour : 30/07/2026 16:02:50
+   Dernière mise à jour : 30/07/2026 19:11:12
    ============================================================ */
 
 window.CONTENU = {
@@ -18,7 +18,18 @@ window.CONTENU = {
   "textes": {},
   "videos": [],
   "audios": [],
-  "boutique": [],
+  "boutique": [
+    {
+      "titre": "Lakhasaye",
+      "prix": "35",
+      "devise": "euro",
+      "categorie": "Lakhasay",
+      "disponible": "oui",
+      "whatsapp": "0678340197",
+      "vendeur": "Ablaye wade",
+      "image": "medias/images/img_3658.jpeg"
+    }
+  ],
   "evenements": [],
   "actualites": [
     {
